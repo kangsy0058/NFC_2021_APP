8bc282d5d52e369c4c4e526474b58daa4a5b6ff0
KIMCHANHEE <hno05039@naver.com>
2021-06-21 22:10:16 +0900

    helloNfcApp

---
+++ lib/treeTest.dart
@@ -0,0 +1,13 @@
+class TestHello{
+
+  String resHello3(String text){
+    return text+text+text;
+  }
+
+  String resHello2(String text){
+    return text+text;
+  }
+
+}
+
+TestHello testHello = TestHello();
