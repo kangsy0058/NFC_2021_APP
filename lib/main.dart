36f43b9bbdc092d9507c3f9752698cfdbe19f68f
YuChaeRin <dbcofls3593@naver.com>
2021-06-22 16:15:45 +0900

    test01

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text('test1',style: TextStyle(fontSize: 30),),
-              Text(testHello.resHello2("과연"),style: TextStyle(fontWeight: FontWeight.bold),),
+              Text(testHello.resHello2("과연?????"),style: TextStyle(fontWeight: FontWeight.bold),),
               Text(testHello.resHello3("뭘까"),style: TextStyle(color: Colors.red),),
             ],
 
