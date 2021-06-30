de4d16d5ef0708d6fd3dd57a4254110f2a10b112
KIMCHANHEE <hno05039@naver.com>
2021-06-30 16:31:52 +0900

    UPDATE:homePage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,27 +13,144 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Container(
         child: Column(
-
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Text("개인 안심번호",style: TextStyle(fontSize: 25),),
-                  Text("디바이스 ID",style: TextStyle(fontSize: 25))
+                  Column(
+                    children: [
+                      Text("개인 안심번호", style: TextStyle(fontSize: 20),),
+                      Text("1234가나", style: TextStyle(fontSize: 20)),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text("디바이스 ID", style: TextStyle(fontSize: 20)),
+                      Text("WSN1234", style: TextStyle(fontSize: 20))
+                    ],
+                  ),
                 ],
               ),
-              Row( mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+              Container(
+                padding: EdgeInsets.fromLTRB(5,30,5,10),
+                margin:  EdgeInsets.fromLTRB(20,20,20,10),
+                child: Column(
                   children: [
-                Text("1234가나",style: TextStyle(fontSize: 25)),
-                Text("WSN1234",style: TextStyle(fontSize: 25))
-              ]
+                    Text("코로나바이러스 감영증 - 19", style: TextStyle(fontSize: 20 )),
+                    Text("06.30 00:00 기준",style: TextStyle(fontSize: 13)),
+                    Container(
+
+                      margin: EdgeInsets.all(1),
+                      padding: EdgeInsets.all(10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            children: [
+                              Text("확진환자"),
+                              Text("794",style: TextStyle(color: Colors.redAccent),),
+                              Text("156,941")
+                          ],),
+                          Column(
+                            children: [
+                              Text("격리해제"),
+                              Text("794",style: TextStyle(color: Colors.blueAccent),),
+                              Text("156,941")
+                            ],),
+                          Column(
+                            children: [
+                              Text("검사 중"),
+                              Text("2,748",style: TextStyle(color: Colors.black),),
+                              Text("156,941")
+                            ],),
+                          Column(
+                            children: [
+                              Text("사망자"),
+                              Text("1",style: TextStyle(color: Colors.black),),
+                              Text("2,018")
+                            ],),
+                        ],
+                      ),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(
+                            width: 1,
+                            color: Colors.white,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(15))
+                      ),
+                    )
+                  ],
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.black12,
+                  border: Border.all(
+                    width: 1,
+                    color: Colors.black12,
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(15))
+                ),
               ),
-
-              Text("코로나 - 19",style: TextStyle(fontSize: 25)),
-
-
-
+              Container(
+                padding: EdgeInsets.fromLTRB(5,30,5,10),
+                margin:  EdgeInsets.fromLTRB(20,20,20,10),
+                child: Column(
+                  children: [
+                    Text("내용들", style: TextStyle(fontSize: 20 )),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      padding: EdgeInsets.all(10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            children: [
+                              Text("확진환자"),
+                              Text("794",style: TextStyle(color: Colors.redAccent),),
+                              Text("156,941")
+                            ],),
+                          Column(
+                            children: [
+                              Text("격리해제"),
+                              Text("794",style: TextStyle(color: Colors.blueAccent),),
+                              Text("156,941")
+                            ],),
+                          Column(
+                            children: [
+                              Text("검사 중"),
+                              Text("2,748",style: TextStyle(color: Colors.black),),
+                              Text("156,941")
+                            ],),
+                          Column(
+                            children: [
+                              Text("사망자"),
+                              Text("1",style: TextStyle(color: Colors.black),),
+                              Text("2,018")
+                            ],),
+                        ],
+                      ),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(
+                            width: 1,
+                            color: Colors.white,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(15))
+                      ),
+                    )
+                  ],
+                ),
+                decoration: BoxDecoration(
+                    color: Colors.black12,
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.black12,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(15))
+                ),
+              ),
 
             ]
         )
